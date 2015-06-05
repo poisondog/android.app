4ca1d7a0e3f82cf162d6517c3c8721abf5210467
poisondog <poisondog@gmail.com>
2015-06-05 14:45:55 +0800

    refactor for AsyncTask to add PostHandler and create AsyncMissionTask let Mission to apply AsyncTask

---
+++ src/poisondog/android/os/AsyncMissionTask.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2015 Adam Huang <poisondog@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package poisondog.android.os;
+
+import poisondog.core.Mission;
+/**
+ * @author Adam Huang <poisondog@gmail.com>
+ */
+public class AsyncMissionTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+	private Mission<Params> mMission;
+	private Exception mException;
+
+	public AsyncMissionTask(Mission<Params> mission) {
+		mMission = mission;
+	}
+
+	@Override
+	protected Result doInBackground(Params... params) {
+		if (params.length > 1)
+			throw new IllegalStateException("AsyncMissionTask input ONE parameter.");
+		Result result = null;
+		try {
+			result = (Result) mMission.execute(params[0]);
+		} catch(Exception e) {
+			mException = e;
+		}
+		return result;
+	}
+}
