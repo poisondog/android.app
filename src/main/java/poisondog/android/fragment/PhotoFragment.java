8c3be278e8015b03e34d728894fe90143a831360
poisondog <poisondog@gmail.com>
2015-09-28 09:49:41 +0800

    refactor RefreshNothing and RefreshOnTop from PhotoFragment

--- src/main/java/poisondog/android/fragment/PhotoFragment.java
+++ src/main/java/poisondog/android/fragment/PhotoFragment.java
@@ -20,14 +20,13 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import poisondog.android.app.DebugFragment;
 import poisondog.android.app.R;
 import poisondog.android.view.PictureWall;
 /**
  * @author poisondog <poisondog@gmail.com>
  */
-public class PhotoFragment extends DebugFragment implements AbsListView.OnScrollListener, SwipeRefreshLayout.OnRefreshListener {
+public class PhotoFragment extends DebugFragment {
 	private PictureWall mPictureWall;
 	private SwipeRefreshLayout mSwipeRefreshLayout;
 
@@ -36,14 +35,14 @@ public class PhotoFragment extends DebugFragment implements AbsListView.OnScroll
 		View v = inflater.inflate(R.layout.photo_grid, container, false);
 		mPictureWall = (PictureWall)v.findViewById(R.id.list);
 		mPictureWall.setFastScrollEnabled(true);
-		mPictureWall.setOnScrollListener(this);
 		mSwipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.photo_grid_swipe_layout);
-		mSwipeRefreshLayout.setOnRefreshListener(this);
 		mSwipeRefreshLayout.setColorSchemeResources(
 				android.R.color.holo_red_light,
 				android.R.color.holo_blue_light,
 				android.R.color.holo_green_light,
 				android.R.color.holo_orange_light);
+		mSwipeRefreshLayout.setOnRefreshListener(new RefreshNothing(mSwipeRefreshLayout));
+		mPictureWall.setOnScrollListener(new RefreshOnTop(mSwipeRefreshLayout));
 		return v;
 	}
 
@@ -53,27 +52,5 @@ public class PhotoFragment extends DebugFragment implements AbsListView.OnScroll
 
 	public PictureWall getPictureWall() {
 		return mPictureWall;
-	}
-
-	@Override
-	public void onRefresh() {
-		mSwipeRefreshLayout.setRefreshing(true);
-		mSwipeRefreshLayout.setRefreshing(false);
-	}
-
-	@Override
-	public void onScrollStateChanged(AbsListView view, int scrollState) {
-	}
-
-	@Override
-	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-		int topPos = -1;
-		if(view.getChildAt(0) != null)
-			topPos = view.getChildAt(0).getTop();
-		if(firstVisibleItem == 0 && topPos == 0) {
-			mSwipeRefreshLayout.setEnabled(true);
-		}else{
-			mSwipeRefreshLayout.setEnabled(false);
-		}
 	}
 }
---
+++ src/main/java/poisondog/android/fragment/RefreshOnTop.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2015 Adam Huang <poisondog@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package poisondog.android.fragment;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.widget.AbsListView;
+/**
+ * @author Adam Huang <poisondog@gmail.com>
+ */
+public class RefreshOnTop implements AbsListView.OnScrollListener {
+	private SwipeRefreshLayout mSwipeRefreshLayout;
+
+	public RefreshOnTop(SwipeRefreshLayout layout) {
+		mSwipeRefreshLayout = layout;
+	}
+
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+	}
+
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		int topPos = -1;
+		if(view.getChildAt(0) != null)
+			topPos = view.getChildAt(0).getTop();
+		if(firstVisibleItem == 0 && topPos == 0) {
+			mSwipeRefreshLayout.setEnabled(true);
+		}else{
+			mSwipeRefreshLayout.setEnabled(false);
+		}
+	}
+}
