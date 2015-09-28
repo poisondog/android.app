3ff97c804a2ca0d7411ca8c1680ea3d242bb3e32
poisondog <poisondog@gmail.com>
2015-09-28 09:31:27 +0800

    fix PhotoFragment refresh not stop problem and refresh on top

--- src/main/java/poisondog/android/fragment/PhotoFragment.java
+++ src/main/java/poisondog/android/fragment/PhotoFragment.java
@@ -20,13 +20,14 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import poisondog.android.app.DebugFragment;
 import poisondog.android.app.R;
 import poisondog.android.view.PictureWall;
 /**
  * @author poisondog <poisondog@gmail.com>
  */
-public class PhotoFragment extends DebugFragment {
+public class PhotoFragment extends DebugFragment implements AbsListView.OnScrollListener, SwipeRefreshLayout.OnRefreshListener {
 	private PictureWall mPictureWall;
 	private SwipeRefreshLayout mSwipeRefreshLayout;
 
@@ -35,7 +36,9 @@ public class PhotoFragment extends DebugFragment {
 		View v = inflater.inflate(R.layout.photo_grid, container, false);
 		mPictureWall = (PictureWall)v.findViewById(R.id.list);
 		mPictureWall.setFastScrollEnabled(true);
+		mPictureWall.setOnScrollListener(this);
 		mSwipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.photo_grid_swipe_layout);
+		mSwipeRefreshLayout.setOnRefreshListener(this);
 		mSwipeRefreshLayout.setColorSchemeResources(
 				android.R.color.holo_red_light,
 				android.R.color.holo_blue_light,
@@ -50,5 +53,27 @@ public class PhotoFragment extends DebugFragment {
 
 	public PictureWall getPictureWall() {
 		return mPictureWall;
+	}
+
+	@Override
+	public void onRefresh() {
+		mSwipeRefreshLayout.setRefreshing(true);
+		mSwipeRefreshLayout.setRefreshing(false);
+	}
+
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+	}
+
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		int topPos = -1;
+		if(view.getChildAt(0) != null)
+			topPos = view.getChildAt(0).getTop();
+		if(firstVisibleItem == 0 && topPos == 0) {
+			mSwipeRefreshLayout.setEnabled(true);
+		}else{
+			mSwipeRefreshLayout.setEnabled(false);
+		}
 	}
 }
