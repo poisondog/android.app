df8656e376e3e9a134fab1d16dc25392cf0560ca
poisondog <poisondog@gmail.com>
2016-01-25 15:19:51 +0800

    add gradle wrapper and MissionHandler

---
+++ src/main/java/poisondog/android/os/MissionHandler.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2016 Adam Huang <poisondog@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package poisondog.android.os;
+
+import android.os.Handler;
+import android.os.Message;
+import poisondog.core.Mission;
+/**
+ * @author Adam Huang <poisondog@gmail.com>
+ */
+public class MissionHandler extends Handler {
+	private Mission<Message> mMission;
+
+	public MissionHandler(Mission<Message> mission) {
+		mMission = mission;
+	}
+
+	@Override
+	public void handleMessage(Message msg) {
+		super.handleMessage(msg);
+		try {
+			mMission.execute(msg);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
