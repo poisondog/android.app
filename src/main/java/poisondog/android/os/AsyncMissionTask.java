16ce608b1b6a4fa2110f1bc2fed98930edca71cb
poisondog <poisondog@gmail.com>
2016-10-06 10:02:58 +0800

    add Progress and Cancel for AsyncMissionTask

--- src/main/java/poisondog/android/os/AsyncMissionTask.java
+++ src/main/java/poisondog/android/os/AsyncMissionTask.java
@@ -16,43 +16,83 @@
 package poisondog.android.os;
 
 import poisondog.core.Mission;
+import poisondog.core.NoMission;
 /**
  * @author Adam Huang <poisondog@gmail.com>
  */
 public class AsyncMissionTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+	private Mission<Progress> mProgress;
 	private Mission<Params> mMission;
 	private Mission<Result> mHandler;
+	private Mission<Params> mCancel;
+	private Params mInput;
 
+	/**
+	 * Constructor
+	 */
 	public AsyncMissionTask(Mission<Params> mission) {
-		this(mission, null);
+		this(mission, new NoMission<Result>());
 	}
 
+	/**
+	 * Constructor
+	 */
 	public AsyncMissionTask(Mission<Params> mission, Mission<Result> handler) {
+		this(mission, new NoMission<Progress>(), handler);
+	}
+
+	/**
+	 * Constructor
+	 */
+	public AsyncMissionTask(Mission<Params> mission, Mission<Progress> progress, Mission<Result> handler) {
 		mMission = mission;
+		mProgress = progress;
 		mHandler = handler;
+		mCancel = new NoMission<Params>();
 	}
 
 	@Override
 	protected Result doInBackground(Params... params) {
 		if (params.length > 1)
 			throw new IllegalStateException("AsyncMissionTask input ONE parameter.");
-		Result result = null;
+		mInput = params[0];
 		try {
-			result = (Result) mMission.execute(params[0]);
+			return (Result)mMission.execute(params[0]);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		return result;
+		return null;
+	}
+
+	@Override
+	protected void onProgressUpdate(Progress... values) {
+		super.onProgressUpdate(values);
+		if (values.length > 1)
+			throw new IllegalStateException("AsyncMissionTask input ONE parameter.");
+		try {
+			mProgress.execute(values[0]);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	protected void onPostExecute(Result result) {
-		if (mHandler == null)
-			return;
 		try {
 			mHandler.execute(result);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+	@Override
+	protected void onCancelled() {
+		super.onCancelled();
+		try {
+			mCancel.execute(mInput);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
